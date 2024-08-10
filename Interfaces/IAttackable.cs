8a26fc454d279b47cbac6430aa898f12d191d59e
Sam Pauwels <sampauwels16@gmail.com>
2024-08-10 16:38:27 +0200

    add IAttack and IAttackable

---
+++ Interfaces/IAttackable.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Interfaces
+{
+    public interface IAttackable
+    {
+        void TakeDamage(int damage);
+    }
+}
+
