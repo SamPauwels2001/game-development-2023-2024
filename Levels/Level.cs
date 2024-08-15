4708777530577d6fcc77fe43a45570154de25ca1
Sam Pauwels <sampauwels16@gmail.com>
2024-08-15 21:45:39 +0200

    add abstract level class

---
+++ Levels/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GameDevProject;
+
+public abstract class Level
+{
+    protected Game1 game;
+
+    public Level(Game1 game)
+    {
+        this.game = game;
+    }
+
+    public virtual void LoadContent() { }
+    public virtual void Update(GameTime gameTime) { }
+    public virtual void Draw(GameTime gameTime) { }
+    public virtual void UnloadContent() { }
+}
