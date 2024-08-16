5162050132a5e249b6b202ff35155c818486f16d
Sam Pauwels <sampauwels16@gmail.com>
2024-08-16 15:48:38 +0200

    add BlockFactory and Block classes

---
+++ LevelDesign/Block.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class Block : IGameObject
+{
+    public Rectangle BoundingBox { get; set; }
+    public bool Passable { get; set; }
+    public Texture2D Texture { get; set; }
+    public CollideWithEvent CollideWithEvent { get; set; }
+
+    public Block(int x, int y, GraphicsDevice graphics)
+    {
+        BoundingBox = new Rectangle(x, y, 10, 10);
+        Passable = false;
+        Texture = new Texture2D(graphics, 1, 1);
+        CollideWithEvent = new NoEvent();
+    }
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, BoundingBox);
+    }
+    public virtual void IsCollidedWithEvent
+    (Character collider)
+    {
+        CollideWithEvent.Execute();
+    }
+}
+
---
+++ LevelDesign/BlockFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class BlockFactory
+{
+    public static Block CreateBlock(
+    string type, int x, int y, GraphicsDevice graphics)
+    {
+        Block newBlock = null;
+        type = type.ToUpper();
+        if (type == "NORMAL")
+        {
+            newBlock = new Block(x, y, graphics);
+        }
+        /*
+        if (type == "TRAP")
+        {
+            newBlock = new TrapBlock(x, y, graphics);
+        }
+        if (type == "SLIME")
+        {
+            newBlock = new SlimeBlock(x, y, graphics);
+        }*/
+        return newBlock;
+    }
+}
