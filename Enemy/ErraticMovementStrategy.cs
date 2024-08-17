b4033feb1c320ed827d00cce1687c5c652bbf817
Sam Pauwels <sampauwels16@gmail.com>
2024-08-17 17:42:51 +0200

    add erratic movement strategy

---
+++ Enemy/ErraticMovementStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using GameDevProject;
+
+public class ErraticMovementStrategy : IMovementStrategy
+{
+    private Random random = new Random();
+
+    public void Move(Enemy enemy, Alice alice, GameTime gameTime)
+    {
+        Vector2 direction = new Vector2((float)(random.NextDouble() - 0.5), (float)(random.NextDouble() - 0.5));
+        direction.Normalize();
+        enemy.Position += direction * enemy.Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+    }
+}
