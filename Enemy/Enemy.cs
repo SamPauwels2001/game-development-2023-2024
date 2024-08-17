17c7be56ca10e4f3513a7c7bc724d54462bfad08
Sam Pauwels <sampauwels16@gmail.com>
2024-08-17 17:53:59 +0200

    implement movement strategies into enemy class

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -3,26 +3,32 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using GameDevProject;
 
-
 public class Enemy
 {
     public Vector2 Position { get; set; }
     public float Speed { get; set; }
+    private IMovementStrategy movementStrategy;
 
     private Texture2D texture;
 
-    public Enemy(Texture2D texture)
+    public Enemy(Texture2D texture, IMovementStrategy strategy)
     {
         this.texture = texture;
+        this.movementStrategy = strategy;
     }
 
-    public void Update(GameTime gameTime)
+    public void Update(GameTime gameTime, Alice alice)
     {
-        //update enemy
+        movementStrategy.Move(this, alice, gameTime);
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.Draw(texture, Position, Color.White);
+    }
+
+    public void SetMovementStrategy(IMovementStrategy strategy)
+    {
+        this.movementStrategy = strategy;
     }
 }
