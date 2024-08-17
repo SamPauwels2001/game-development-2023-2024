cdb1e686970bb3c43f92a1286b461575f4e06e3a
Sam Pauwels <sampauwels16@gmail.com>
2024-08-17 17:22:57 +0200

    add enemy class

---
+++ Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+public class Enemy
+{
+    public Vector2 Position { get; set; }
+    public float Speed { get; set; }
+
+    private Texture2D texture;
+
+    public Enemy(Texture2D texture)
+    {
+        this.texture = texture;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        //update enemy
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(texture, Position, Color.White);
+    }
+}
