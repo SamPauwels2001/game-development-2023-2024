0eeb687afa703e0a0915b6301a83093f0433b1f0
Sam Pauwels <sampauwels16@gmail.com>
2024-08-18 12:39:46 +0200

    add grass blocks to level3

--- Levels/Level3.cs
+++ Levels/Level3.cs
@@ -6,25 +6,62 @@ using Microsoft.Xna.Framework.Content;
 
 public class Level3 : Level
 {
+    private Block[,] gameBoard;
+
     public Level3(Game1 game, SpriteBatch spriteBatch, ContentManager content)
         : base(game, spriteBatch, content) { }
 
     public override void LoadContent()
     {
         base.LoadContent();
-        // Load level-specific content
+
+        int tileWidth = 64;
+        int tileHeight = 64;
+        int numTilesX = (int)Math.Ceiling((double)Game1.ScreenWidth / tileWidth);
+        int numTilesY = (int)Math.Ceiling((double)Game1.ScreenHeight / tileHeight);
+
+        gameBoard = new Block[numTilesX, numTilesY];
+
+        for (int x = 0; x < numTilesX; x++)
+        {
+            for (int y = 0; y < numTilesY; y++)
+            {
+                int posX = x * tileWidth;
+                int posY = y * tileHeight;
+
+                if (posX >= Game1.ScreenWidth || posY >= Game1.ScreenHeight)
+                    continue;
+
+                gameBoard[x, y] = BlockFactory.CreateBlock(
+                    "GRASS", posX, posY, tileWidth, tileHeight,
+                    tileSet, grassSourceRectangle
+                );
+            }
+        }
     }
 
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
-        // Update level
+        // Update level 1
     }
 
     public override void Draw(GameTime gameTime)
     {
+        spriteBatch.Begin();
+
+        for (int x = 0; x < gameBoard.GetLength(0); x++)
+        {
+            for (int y = 0; y < gameBoard.GetLength(1); y++)
+            {
+                gameBoard[x, y].Draw(spriteBatch);
+            }
+        }
+
+        spriteBatch.End();
+
         base.Draw(gameTime);
-        // Draw level
+
     }
 
     public override void UnloadContent()
